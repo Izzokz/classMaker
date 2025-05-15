ed1566923b9c055413270397f29d52a7311aec37
Kylian Zhen-Claverie <kzhen-cl@2E3.42angouleme.fr>
2025-05-15 17:16:48 +0200

    Init

---
+++ Makefile
@@ -0,0 +1,30 @@
+NAME = class
+
+SRC = classMaker.cpp
+OBJ = $(SRC:.cpp=.o)
+DEP = $(SRC:.cpp=.d)
+
+CCA = c++ -std=c++98 -Werror -Wall -Wextra -MP -MMD -g3
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	@$(CCA) $(OBJ) -o $(NAME)
+	@printf "Build Complete !\n"
+
+%.o: %.cpp
+	@$(CCA) -o $@ -c $<
+
+clean:
+	@rm -f $(OBJ) $(DEP)
+	@printf "Objects Cleaned !\n"
+
+fclean:
+	@rm -f $(OBJ) $(NAME) $(DEP)
+	@printf "Cleaned !\n"
+
+re: fclean all
+
+.PHONY: all clean fclean re
+
+-include $(DEP)
---
+++ classMaker.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   classMaker.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzhen-cl <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 15:18:40 by kzhen-cl          #+#    #+#             */
+/*   Updated: 2025/05/15 15:18:41 by kzhen-cl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fstream>
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	while (--argc)
+	{
+		++argv;
+		std::ofstream	hpp(((std::string)*argv + ".hpp").c_str());
+		if (!hpp.is_open())
+		{
+			std::cout << "\e[31;1m[Fatal Error] Can't open " << (std::string)*argv + ".hpp\e[0m\n";
+			return (2);
+		}
+		std::ofstream	cpp(((std::string)*argv + ".cpp").c_str());
+		if (!cpp.is_open())
+		{
+			std::cout << "\e[31;1m[Fatal Error] Can't open " << (std::string)*argv + ".cpp\e[0m\n";
+			hpp.close();
+			return (2);
+		}
+		hpp << "#pragma once\n\nclass\t" << *argv << "\n{\n\tpublic:\n\t\t"
+			<< *argv << "(void);\n\t\t" << *argv << "(const " << *argv << " &cpy);\n\t\t"
+			<< *argv << "\t&operator=(const " << *argv << " &cpy);\n\t\t~"
+			<< *argv << "(void);\n}\n";
+		hpp.close();
+		cpp << "#include \"" << *argv << ".hpp\"\n\n"
+			<< *argv << "::" << *argv << "(void)\n{\n\t"
+			<< "std::cout << \"" << *argv << " default constructor called\\n\";\n}\n\n"
+			<< *argv << "::" << *argv << "(const " << *argv << " &cpy)\n{\n\t"
+			<< "std::cout << \"" << *argv << " copy constructor called\\n\";\n}\n\n"
+			<< *argv << "\t&" << *argv << "::operator=(const " << *argv << " &cpy)\n{\n\t"
+			<< "std::cout << \"" << *argv << " copy assignment operator called\\n\";\n}\n\n"
+			<< *argv << "::~" << *argv << "(void)\n{\n\t"
+			<< "std::cout << \"" << *argv << " destructor called\\n\";\n}\n";
+	}
+}
