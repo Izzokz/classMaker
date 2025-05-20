0fffde012dae623b49525da01408ea71ce1f113c
Kylian Zhen-Claverie <kzhen-cl@1C2.42angouleme.fr>
2025-05-20 20:55:51 +0200

    class can be used without argument so it shows help

--- classMaker.cpp
+++ classMaker.cpp
@@ -14,7 +14,20 @@
 
 bool	gDebug = 0;
 
-void	parseCompose(CMCompose &constructor, std::string &rules)
+static void	printHelp(void)
+{
+	std::cout << "Usage:\n\t- class -d <format1> <format2> <format3>... # Fills all classes with debug messages (Constructors and Destructor)\n\t- "
+		<< "class <format1> <format2> <format3>... # Basic class making\nFormats:\n\t- ClassName # Creates a class\n\t- \"ClassName ; <operator1> <operator2>...\""
+		<< " # Creates a class with operators\n\t- \"ClassName : ParentClass1, ParentClass2, ...\" # Creates a class with inheritance /!\\ NOT IMPLEMENTED YET /!\\\n\t-"
+		<< " \"ClassName : ParentClass, ... ; <operator> ...\" # Creates a class with both inheritance and operators"
+		<< " /!\\ NOT IMPLEMENTED YET /!\\\nOperators:\n\t- op+ op- op/ op* op% op++ ++op op-- --op # Arithmetic and Increment/Decrement operators\n\t- op== op!= op> op< op>= op<="
+		<< " # Comparison operators\n\t- op&& op|| op! # Logical operatos\n\t- op& op| op^ op~ op<< op>> # Bitwise operators\n\t- op&= op|= op^= op<<= op>>="
+		<< " # Bitwise compound assignment operators\n\t- op+= op-= op/= op*= op%= # Compound assignment operators\n\t- op. op-> op? op: op, op() op[] # Miscellaneous operators"
+		<< "\n\t- opnew opdelete opnew[] opdelete[] # Memory management operators\nExtra Information:\n\t- The setup is public only (no protected, no private...)\n\t-"
+		<< " The program is case sensitive (\"Class:Parent,ParentTheSecond ; op++op--\" will create a class named \"Class:Parent,ParentTheSecond\" with no operator)\n";
+}
+
+static void	parseCompose(CMCompose &constructor, std::string &rules)
 {
 	size_t			semi = rules.find(';');
 
@@ -29,6 +42,8 @@ void	parseCompose(CMCompose &constructor, std::string &rules)
 
 int	main(int argc, char *argv[])
 {
+	if (argc == 1)
+		printHelp();
 	if (argc > 1 && (std::string)*(argv + 1) == "-d")
 	{
 		gDebug = 1;
